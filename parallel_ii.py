3b63bab14dad0e1d0f905f7bdafa495a280edb3e
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2020-05-07 12:41:40 +0300

    Fix parallel_ii.py - final realization with overriding Process class

--- parallel_ii.py
+++ parallel_ii.py
@@ -1,15 +1,29 @@
-from multiprocessing import Process, Manager
+from multiprocessing import Process
 import os
 from time import time
 
-class InvertedIndex:
+class MyProcess(Process):
 
-    def __init__(self, directory, num_of_processes):
-        m = Manager()
-        self.index_dictionary = m.Queue()
+    def __init__(self, directory, files):
+        Process.__init__(self)
         self.directory = directory
-        self.num_of_processes = num_of_processes
-        self.files = os.listdir(directory)
+        self.files = files
+        self.process_storage = {}
+
+
+    def open_files(self):
+        '''
+        read all files in a current process's chunk
+        split text in every file with ' '
+        call normalize_word
+        call inverted_index
+        '''
+        for file in self.files:
+            with open(self.directory + file) as file_handler:
+                for text in file_handler:
+                    text = text.split(' ')
+                    text = [self.normalize_word(x) for x in text]
+                    self.inverted_index(text, file)
 
 
     def normalize_word(self, word):
@@ -17,8 +31,7 @@ class InvertedIndex:
         word -> lower()
         remove signs
         '''
-
-        signs = [',', '!', '/', '?', '.', '(', ')', "'", '"', '<', '>']
+        signs = [',', '!', '/', '?', '.', '(', ')', "'", '"', '<', '>', ':', ';', '_']
         word = word.lower()
         if word:
             for sign in signs:
@@ -27,67 +40,68 @@ class InvertedIndex:
         return word
 
 
-    def divide_data(self):
-        N = int(len(self.files) / self.num_of_processes)
-        result = [x * N for x in range(self.num_of_processes)]
-        result.append(None)
-
-        return result
-
-
-    def run(self):
-        processes = []
-        chunks = self.divide_data()
-
-        for i in range(self.num_of_processes):
-            p = Process(target=self.open_files, args=(self.files[chunks[i]:chunks[i+1]],))
-            processes.append(p)
-
-        [x.start() for x in processes]
-        [x.join() for x in processes]
-
-
-    def open_files(self, files_block):
-        for file in files_block:
-            with open(self.directory + file) as file_handler:
-                for text in file_handler:
-                    text = text.split(' ')
-                    text = [self.normalize_word(x) for x in text]
-                    self.inverted_index(text, file)
-
-
     def inverted_index(self, line, docID):
         '''
         add to index_dictionary: keys - 'word', values - (docID, position in file)
         '''
         position = 0
-        local_dict = {}
         for word in line:
-            if word not in local_dict.keys():
-                local_dict[word] = [(docID, position)]
+            if word not in self.process_storage.keys():
+                self.process_storage[word] = [(docID, position)]
             else:
-                local_dict[word] += ([(docID, position)])
+                self.process_storage[word] += ([(docID, position)])
             position += 1
 
-        self.index_dictionary.put(local_dict, timeout=None)
+
+    def run(self):
+        self.open_files()
+        return self.process_storage
 
 
-    def save_index_dictionary(self):
-        '''
-        save index_dictionary as txt file
-        '''
-        with open('inverted_index_parallel.txt', 'a') as file:
-            while not self.index_dictionary.empty():
-                file.write(str(self.index_dictionary.get(timeout=None)))
+def divide_data(files, num_of_processes):
+    '''
+    divide data into similar chunks
+    '''
+    chunk_size = int(len(files) / num_of_processes)
+    division_list = [x * chunk_size for x in range(num_of_processes)]
+    division_list.append(None)
+
+    return division_list
 
 
-    def __call__(self):
-        self.run()
-        self.save_index_dictionary()
+def merge_dictionaries(local_dictionaries):
+    '''
+    merge local dictionaries returned from processes into one
+    '''
+    root = local_dictionaries[0]
+    for local_dict in local_dictionaries[1:]:
+        for key in local_dict.keys():
+            if key in root.keys():
+                root[key] += local_dict[key]
+            else:
+                root[key] = local_dict[key]
+
+    return root
 
 
 if __name__ == '__main__':
-    ii = InvertedIndex(directory='C:/Users/Alisa/Desktop/test/', num_of_processes=2)
+
+    directory = 'C:/Users/Alisa/Desktop/test/'
+    files = os.listdir(directory) # list of file names ['8251_3.txt', '8251_7.txt', ...]
+    num_of_processes = 2
+
     start_time = time()
-    ii()
-    print(time() - start_time)
+    division_list = divide_data(files, num_of_processes)
+    local_dictionaries = []
+    processes = []
+
+    for i in range(num_of_processes):
+        p = MyProcess(directory, files[division_list[i]:division_list[i+1]])
+        processes.append(p)
+
+    local_dictionaries += [x.run() for x in processes]
+    inverted_index = merge_dictionaries(local_dictionaries)
+
+    print(f'Parallel result: {time() - start_time}')
+    with open('inverted_index_parallel.txt', 'a') as file:
+        file.write(str(inverted_index))
